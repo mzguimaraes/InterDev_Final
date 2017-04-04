e54203f3782cbade9783ba73e91ff3c4e726ab82
CamDHall <camerondylan.h@gmail.com>
2017-04-04 10:17:14 -0400

    Smaller arena and fixed powerups positions

--- Assets/Scripts/PowerUps.cs
+++ Assets/Scripts/PowerUps.cs
@@ -5,22 +5,24 @@ using UnityEngine;
 public class PowerUps : MonoBehaviour {
 
     public GameObject powerup_1, powerup_2, powerup_3, powerup_4;
+    Vector3 Pos;
 
 	void Start () {
         int choice = Random.Range(1, 4);
+        Pos = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
 
 		if(choice == 1)
         {
-            Instantiate(powerup_1, transform.position, Quaternion.identity);
+            Instantiate(powerup_1, Pos, Quaternion.identity);
         } else if (choice == 2)
         {
-            Instantiate(powerup_2, transform.position, Quaternion.identity);
+            Instantiate(powerup_2, Pos, Quaternion.identity);
         } else if (choice == 3)
         {
-            Instantiate(powerup_3, transform.position, Quaternion.identity);
+            Instantiate(powerup_3, Pos, Quaternion.identity);
         } else
         {
-            Instantiate(powerup_4, transform.position, Quaternion.identity);
+            Instantiate(powerup_4, Pos, Quaternion.identity);
         }
 	}
 	
