44ef51171e207080383a7cfa2b8f8e82aa5ee285
CamDHall <camerondylan.h@gmail.com>
2017-04-06 14:09:11 -0400

    Powerup placements

---
+++ Assets/Scripts/Powerups/FreezeTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeTime : MonoBehaviour {
+    	
+	void Update () {
+
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            Destroy(coll.gameObject);
+            PowerUpManager._freezeTimer = Time.timeSinceLevelLoad + 3.0f;
+        }
+    }
+}
