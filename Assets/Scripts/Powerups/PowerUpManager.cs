9f3b0876fc9f581dd7e1d52d4bd394c810400ef4
CamDHall <camerondylan.h@gmail.com>
2017-04-06 14:09:11 -0400

    Powerup placements

---
+++ Assets/Scripts/Powerups/PowerUpManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour {
+
+    public GameObject freeze, damage, speed, jump;
+
+    public static bool freezeExist, damageExist, speedExist, jumpExist;
+    public static float _freezeTimer, _damageTimer, _speedTimer, _jumpExist; 
+
+	void Start () {
+        _freezeTimer = 0;
+        _damageTimer = 0;
+        _speedTimer = 0;
+        _jumpExist = 0;
+
+        freezeExist = true;
+        damageExist = true;
+        speedExist = true;
+        jumpExist = true;
+	}
+	
+	void Update () {
+	    if(!freezeExist && _freezeTimer < Time.timeSinceLevelLoad)
+        {
+            var freezePowerUp = Instantiate(freeze);
+            freeze.transform.SetParent(transform);
+            Debug.Log("Freeze Spawn");
+        }
+	}
+}
