8e85738b3541f0153725f513684334ea4cf780c2
CamDHall <camerondylan.h@gmail.com>
2017-04-18 14:34:27 -0400

    Speed and jump boost

--- Assets/Scripts/Powerups/DamageBoost.cs
+++ Assets/Scripts/Powerups/DamageBoost.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class DamageBoost : MonoBehaviour {
 
+    OurPlayerController ourPlayerController;
+
+    void Start()
+    {
+        ourPlayerController = GameObject.FindGameObjectWithTag("Player").GetComponent<OurPlayerController>();
+    }
+
     void OnCollisionEnter(Collision coll)
     {
         if (coll.gameObject.tag == "Player")
         {
             PowerUpManager._damageTimer = Time.timeSinceLevelLoad + (10.0f + PowerUpManager.damagerCounter);
             PowerUpManager.damageExist = false;
-            Destroy(this.gameObject);
+
+            StartCoroutine("DamageEffect");
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            gameObject.GetComponent<SphereCollider>().enabled = false;
         }
+    }
+
+    IEnumerator DamageEffect()
+    {
+        ourPlayerController.jumpMagnitude = 40f;
+        yield return new WaitForSeconds(15);
+
+        DamageReset();
+    }
+
+    void DamageReset()
+    {
+        ourPlayerController.jumpMagnitude = 15f;
     }
 }
