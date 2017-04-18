cbf49f6696312c96b2dc147f6beddd0fb2275dac
CamDHall <camerondylan.h@gmail.com>
2017-04-18 15:56:49 -0400

    Jump fix

--- Assets/Scripts/OurPlayerController.cs
+++ Assets/Scripts/OurPlayerController.cs
@@ -11,54 +11,7 @@ public class OurPlayerController : MonoBehaviour {
 
     public float forceVariable = 50f;
 
-	public float jumpMagnitude = 15f;
-
-    // Raycasting
-    bool isGrounded;
-
-    void Start () {
-        rbody = GetComponent<Rigidbody>(); // Cache rigidbody
-    }
-	
-	void Update () {
-        float horizontal = Input.GetAxis("Horizontal"); // A/D
-        float vertical = Input.GetAxis("Vertical"); // W/S
-
-        inputVector = new Vector3(horizontal, 0, vertical);
-
-        // Turning
-        transform.Rotate(0f, Input.GetAxis("Mouse X") * Time.deltaTime * 300f, 0);
-
-        // Jumping
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            inputVector.y = jumpMagnitude;
-        }
-    }
-
-    void FixedUpdate()
-    {
-        rbody.AddRelativeForce(Vector3.up * inputVector.y * 10f);
-
-        if (rbody.velocity.magnitude < maxSpeed)
-        {
-            rbody.AddRelativeForce(inputVector * forceVariable);
-        }
-
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f);
-    }
-}
-
-public class OurPlayerController : MonoBehaviour {
-
-    Rigidbody rbody;
-    Vector3 inputVector;
-
-    public float maxSpeed = 5f;
-
-    public float forceVariable = 10f;
-
-	public float jumpMagnitude = 15f;
+	public float jumpMagnitude = 5f;
 
     // Raycasting
     bool isGrounded;
