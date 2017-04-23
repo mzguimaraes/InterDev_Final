e4846841582c61854b834704483f2be0c99ad698
Marcus Guimaraes <mguimaraes42@gmail.com>
2017-04-22 22:25:59 -0400

    added crosshair

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Crosshair : MonoBehaviour {
+
+	//will be drawn 4 times to create crosshair
+	public RawImage CrosshairSegment;
+	//distance from CrosshairCenter each segment will be drawn at
+	public float CrosshairDelta = 13f;
+	public Vector2 CrosshairCenter = Vector2.zero;
+
+	private RawImage CrosshairUp;
+	private RawImage CrosshairLeft;
+	private RawImage CrosshairDown;
+	private RawImage CrosshairRight;
+	
+	void Start () {
+		//create crosshair
+		CrosshairUp = Instantiate(CrosshairSegment, transform, false);
+		CrosshairUp.rectTransform.anchoredPosition = new Vector2(0f, CrosshairDelta);
+
+		CrosshairLeft = Instantiate(CrosshairSegment, transform, false);
+		CrosshairLeft.rectTransform.anchoredPosition = new Vector2(CrosshairDelta, 0f);
+		CrosshairLeft.rectTransform.Rotate(0f, 0f, 90f);
+
+		CrosshairDown = Instantiate(CrosshairSegment, transform, true);
+		CrosshairDown.rectTransform.anchoredPosition = new Vector2(0f, -CrosshairDelta);
+
+		CrosshairRight = Instantiate(CrosshairSegment, transform, false);
+		CrosshairRight.rectTransform.anchoredPosition = new Vector2(-CrosshairDelta, 0f);
+		CrosshairRight.rectTransform.Rotate(0f, 0f, -90f);
+	}
+}
---
+++ Assets/Scripts/RaycastShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class RaycastShoot : MonoBehaviour {
+
+	public Crosshair crosshairPrefab;
+
+	void Shoot () {
+		//shoots a raycast from Canvas point (0,0)
+		//(the center of the crosshair)
+		//and deals damage to obj hit if damageable
+	}
+
+	void Start () {
+		//create crosshair and parent it to canvas
+		Canvas UICanvas = FindObjectOfType<Canvas>();
+		Instantiate(crosshairPrefab, UICanvas.transform, false);
+	}
+	
+	void Update () {
+		
+	}
+}
