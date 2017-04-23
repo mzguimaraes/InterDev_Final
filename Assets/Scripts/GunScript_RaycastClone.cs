54fb9b361732443cd1f2675c63231ec12ba29772
Marcus Guimaraes <mguimaraes42@gmail.com>
2017-04-22 23:59:07 -0400

    raycast shoot implemented

---
+++ Assets/Scripts/GunScript_RaycastClone.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RaycastShoot))]
+public class GunScript_RaycastClone : MonoBehaviour {
+	//took Nick's GunScript and hooked it into my raycast shooting implementation -marcus
+
+	bool HaveGun = true;
+	bool Pistol = true;
+	bool Revolver = false;
+	bool Rifle = false;
+	public GameObject PistolGun;
+
+	public int damageAmount = 1;
+
+	private RaycastShoot firingTrigger;
+
+	// Use this for initialization
+	void Start () {
+		firingTrigger = GetComponent<RaycastShoot>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//See if the player has a gun at all and if they press the LMB
+		if (HaveGun && Input.GetKeyDown (KeyCode.Mouse0)) {
+			//if they have a pistol, shoot a pistol bullet, same goes for the revolver and rifle variables
+			firingTrigger.Shoot(damageAmount);
+		}
+	}
+		
+	void OnTriggerEnter(Collider other){
+		//if the player comes across a gun with the tag "gun" they will pick it up, have it parented to the players "CurrentGun" child and set it to the transform
+		if (other.gameObject.tag == "Gun") {
+			if (this.transform.childCount > 0) {
+				Destroy (this.transform.GetChild (0).gameObject);
+			}
+			HaveGun = true;
+            Behaviour halo = (Behaviour)other.gameObject.GetComponent("Halo");
+            halo.enabled = false;
+			other.transform.SetParent (transform);
+			other.transform.rotation = this.transform.rotation;
+			other.transform.position = this.transform.position;
+			//each gun has their name that coorrelates with the boolean that works within update to shoot the proper bullet
+			if (other.gameObject.name == "PistolGun") {
+//				Debug.Log ("PISTOL");
+				Pistol = true;
+				Revolver = false;
+				Rifle = false;
+			}
+			if (other.gameObject.name == "RevolverGun") {
+//				Debug.Log ("Revolver");
+				Revolver = true;
+				Pistol = false;
+				Rifle = false;
+			}
+			if (other.gameObject.name == "RifleGun") {
+//				Debug.Log ("RIFLE");
+				Rifle = true;
+				Pistol = false;
+				Revolver = false;
+			}
+		}
+	}
+}
--- Assets/Scripts/RaycastShoot.cs
+++ Assets/Scripts/RaycastShoot.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class RaycastShoot : MonoBehaviour {
+	//TODO: if team likes this, refactor for heavy use
 
 	public Crosshair crosshairPrefab;
 
-	void Shoot () {
-		//shoots a raycast from Canvas point (0,0)
+	private Canvas UICanvas;
+
+	public void Shoot (int damageAmount) {
+		//shoots a raycast from Viewport point (.5, .5, 0)--the center of the screen
 		//(the center of the crosshair)
 		//and deals damage to obj hit if damageable
+
+		Ray bulletPath = Camera.main.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+		RaycastHit rch;
+		Physics.Raycast(bulletPath, out rch);
+		HealthSystem healthObj = rch.collider.gameObject.GetComponent<HealthSystem>();
+		if (healthObj != null) {
+			healthObj.TakeDamage(damageAmount);
+		}
 	}
 
 	void Start () {
 		//create crosshair and parent it to canvas
-		Canvas UICanvas = FindObjectOfType<Canvas>();
+		UICanvas = FindObjectOfType<Canvas>();
 		Instantiate(crosshairPrefab, UICanvas.transform, false);
 	}
 	
