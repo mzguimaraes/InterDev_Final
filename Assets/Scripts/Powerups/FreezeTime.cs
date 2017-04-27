c3e720eca18326664c5023f2857f7327956c8f07
CamDHall <camerondylan.h@gmail.com>
2017-04-27 16:03:12 -0400

    Powerups Final

--- Assets/Scripts/Powerups/FreezeTime.cs
+++ Assets/Scripts/Powerups/FreezeTime.cs
@@ -13,7 +13,7 @@ public class FreezeTime : MonoBehaviour {
 
             StartCoroutine("FreezeEffect");
             gameObject.GetComponent<MeshRenderer>().enabled = false;
-            gameObject.GetComponent<SphereCollider>().enabled = false;
+            gameObject.GetComponent<MeshCollider>().enabled = false;
         }
     }
 
