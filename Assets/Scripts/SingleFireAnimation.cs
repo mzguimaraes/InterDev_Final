77e8100ab55aff6ca679cfaee2eb3bc32483ef98
Nicholas Neissa <nan278@nyu.edu>
2017-05-02 16:30:33 -0400

    All Guns with Animations and Animation Scripts attached

---
+++ Assets/Scripts/SingleFireAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleFireAnimation : MonoBehaviour {
+	Animator GunAnimator;
+	// Use this for initialization
+	void Start () {
+		GunAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			GunAnimator.Play("Recoil");
+		}
+
+	}
+}
